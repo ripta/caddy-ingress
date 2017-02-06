4f96a7e0ebbb4d2e431fa8437f0b3a911d3f44ea
Ripta Pasay <ripta@users.noreply.github.com>
2017-02-05 23:50:27 -0800

    controller: add healthz server

--- controller.go
+++ controller.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/golang/glog"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/kubernetes/pkg/healthz"
 )
 
 type CaddyIngressController struct {
@@ -17,8 +22,28 @@ func NewIngressController(clientset *kubernetes.Clientset, svcFallback string) (
 }
 
 func (c *CaddyIngressController) RunHealthz(port int) {
-	_ = port
+	mux := http.NewServeMux()
+	healthz.InstallHandler(mux)
+
+	mux.HandleFunc("/stop", func(w http.ResponseWriter, r *http.Request) {
+		c.Stop()
+	})
+
+	mux.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Caddy Ingress Controller version %v", version)
+	})
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%v", port),
+		Handler: mux,
+	}
+
+	glog.Fatal(server.ListenAndServe())
 }
 
 func (c *CaddyIngressController) WatchNamespace(namespace string) {
+}
+
+func (c *CaddyIngressController) Stop() {
 }
